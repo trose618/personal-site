8b96787db65d574dc07615fdeced69670fb24646
trose618 <trose618@gmail.com>
2019-05-23 15:19:42 -0400

    add js for rendering mobile menu with burger click

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 //do this when document is loaded
 document.addEventListener("DOMContentLoaded", () => {
 
+    let burger = document.querySelector('.burger');
+    let mobileMenu = document.querySelector('.menu-holder');
+
+    burger.addEventListener('click', () => {
+        mobileMenu.style.display = mobileMenu.style.display === "none" ? 'block' : "none"
+    })
+
+
+    //setting up menu display
+    mobileMenu.style.display = "none";
+    mobileMenu.addEventListener('click', () => {
+        console.log('clicked inside menu')
+    })
 
 })
 
