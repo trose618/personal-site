39620634bf3ae72eca13c19bbebba7dc1c106a33
trose618 <trose618@gmail.com>
2019-06-03 06:38:27 -0400

    added dynamic footer content

--- index.js
+++ index.js
@@ -1,19 +1,53 @@
-//do this when document is loaded
 document.addEventListener("DOMContentLoaded", () => {
 
     let burger = document.querySelector('.burger');
     let mobileMenu = document.querySelector('.menu-holder');
+    let currentPage = document.getElementById('current-page');
+    let footerList = document.querySelector('.footer-menu-list');
+
+    footerList.innerHTML = toggleFooterList("About Me");
 
     burger.addEventListener('click', () => {
-        mobileMenu.style.display = mobileMenu.style.display === "none" ? 'block' : "none"
+        // mobileMenu.style.display = mobileMenu.style.display === "none" ? 'block' : "none";
+        document.querySelector('.menu-holder').classList.toggle('active');
+    })
+
+    mobileMenu.addEventListener('click', (e) => {
+
+        e.preventDefault();
+        //get page name that was clicked
+        let page = e.target.innerText;
+
+        currentPage.innerText = page;
+
+        footerList.innerHTML = toggleFooterList(page);
+
+        document.querySelector('.menu-holder').classList.toggle('active');
     })
 
 
-    //setting up menu display
-    mobileMenu.style.display = "none";
-    mobileMenu.addEventListener('click', () => {
-        console.log('clicked inside menu')
-    })
+    function toggleFooterList(page) {
+        if (page === 'About Me') {
+            return `<li class="footer-menu-item">Bio</li>
+                    <li class="footer-menu-item">Skills</li>
+                    <li class="footer-menu-item">Hobbies</li>`
+        }
+        if (page === 'Resume') {
+            return `<li class="footer-menu-item">Bio</li>
+                    <li class="footer-menu-item">Projects</li>
+                    <li class="footer-menu-item">Skills</li>
+                    <li class="footer-menu-item">Experiences</li>`
+        }
+        if (page === 'Projects') {
+            return `<li class="footer-menu-item">Squash Buddy</li>
+                    <li class="footer-menu-item">Welcome Board</li>
+                    <li class="footer-menu-item">Coach Me</li>`
+        }
+        if (page === 'Blog') {
+            return `<li class="footer-menu-item">Latest</li>
+                    <li class="footer-menu-item">First</li>`
+        }
+    }
 
 })
 
